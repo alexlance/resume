6f53ca9365dd419a9a4ca4e29874bbf28ba90f29
Alex Lance <code@alexlance.com>
2026-03-09 09:51:14 +1100

    Remove old test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DATE=$(date "+%A %D")
-export MY_EMAIL="@alexlance.com"
-export JOB_TITLE="*Infrastructure and automation engineer* with ......"
-export JOB_REASON="
-"
-envsubst < resume.md.tpl > resume.md && gimli -file resume.md && rm resume.md
